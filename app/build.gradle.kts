f9a96c4b01dd8031789b25af5b2044394cc7b46d
SergioMartinezAlonso <sergioma2004.sma@gmail.com>
2025-12-14 22:42:34 +0100

    Implementada la vista temario
    He creado la vista de "Temario", para que sea lo más fiel posible a nuestro diseño de figma. Incluye las asignaturas y la sección para subir nuevos archivos (sin la lógica).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
